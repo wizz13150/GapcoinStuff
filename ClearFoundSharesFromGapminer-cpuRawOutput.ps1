2f40c982bcb5ed307a6cd2cf0406889ad04310e8
Wizz_^ <lewizard@hotmail.fr>
2021-02-27 23:16:36 +0100

    Add files via upload

---
+++ ClearFoundSharesFromGapminer-cpuRawOutput.ps1
@@ -0,0 +1,4 @@
+﻿Get-Content "C:\Users\Wizz\Desktop\Output - Copie.txt" | Where { $_ -match "accepted" } | ForEach {
+$stuff = $_ -replace "  =>  accepted" -replace " Found Share:"," ;" -replace "]" -replace "\.",","
+$stuff.Remove(0,1)
+} | Set-Content C:\Users\Wizz\Desktop\Result.txt
