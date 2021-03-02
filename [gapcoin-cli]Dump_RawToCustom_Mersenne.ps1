35cfad0826876cf2210bce7146e48ed78fbc3c4b
Wizz_^ <lewizard@hotmail.fr>
2021-03-02 01:44:57 +0100

    Update [gapcoin-cli]Dump_RawToCustom_Mersenne.ps1

--- [gapcoin-cli]Dump_RawToCustom_Mersenne.ps1
+++ [gapcoin-cli]Dump_RawToCustom_Mersenne.ps1
@@ -65,7 +65,7 @@
     ###############################################
     #If Clean DumpBlocks file exist, rename with formatted date
     If ((Test-Path -Path "$($Path)$($DumpCustom)" -PathType Leaf) -eq $True){
-    $Null=Rename-Item -Path "$($Path)$($DumpCustom)" -NewName "$($Path)$($DumpMersenne)_$($FDate).csv" -Force -ErrorAction Ignore}
+    $Null=Rename-Item -Path "$($Path)$($DumpCustom)" -NewName "$($Path)$($DumpCustom)_$($FDate).csv" -Force -ErrorAction Ignore}
     for($c = 0; $c -lt $height.Count; $c++){
     #Adapt this to selection or swap columns
     ('{0}{1}{2}{3}{4}{5}{6},{7}{8}' -f $height[$c],$Date[$c],$nonce[$c],$adder[$c],$difficulty[$c],$shift[$c],$Merit6[$c],$Gap[$c],$gapstart[$c])|Add-Content "$($Path)$($DumpCustom)"}
