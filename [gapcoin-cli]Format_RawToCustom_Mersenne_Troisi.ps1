117be6d46d960e30fc7651e3b63d776cd145b23f
Wizz_^ <lewizard@hotmail.fr>
2021-03-06 19:49:23 +0100

    Update and rename [gapcoin-cli]Dump_RawToCustom_Mersenne_Troisi.ps1 to [gapcoin-cli]Format_RawToCustom_Mersenne_Troisi.ps1

--- [gapcoin-cli]Format_RawToCustom_Mersenne_Troisi.ps1
+++ [gapcoin-cli]Format_RawToCustom_Mersenne_Troisi.ps1
@@ -1,8 +1,9 @@
     #Line to eventually edit : 4,5,128.
     #Line to eventually comment/uncomment for a custom format : 14 to 80.
     #Dir and Files paths
-    $Path="C:\Temp\test\old\";$Raw="Dump_LastBlocks.csv"
-    $DumpMersenne="Dump_LastBlocks_Mersenne.csv";$DumpCustom="Dump_LastBlocks_Custom.csv"
+    $Path="C:\Temp\test\old\";$Raw="Dump.csv"
+    $DumpMersenne="$($Path)Dump_Mersenne.csv";$DumpCustom="$($Path)Dump_Custom.csv"
+    $DumpTroisi="$($Path)Dump_Troisi.csv"
     $FDate=((Get-Date) -replace " ","_" -replace ":" -replace "/")
       
 
@@ -64,12 +65,12 @@
     ######Custom Format Output from RAW datas######
     ###############################################
     #If Clean DumpBlocks file exist, rename with formatted date
-    If ((Test-Path -Path "$($Path)$($DumpCustom)" -PathType Leaf) -eq $True){
-    $Null=Rename-Item -Path "$($Path)$($DumpCustom)" -NewName "$($Path)$($DumpCustom)_$($FDate).csv" -Force -ErrorAction Ignore}
+    If ((Test-Path -Path $DumpCustom -PathType Leaf) -eq $True){
+    $Null=Rename-Item -Path $DumpCustom -NewName "$($DumpCustom)_$($FDate).csv" -Force -ErrorAction Ignore}
     for($c = 0; $c -lt $height.Count; $c++){
     #Adapt this to selection or swap columns
     ('{0}{1}{2}{3}{4}{5}{6},{7}{8}' -f $height[$c],$Date[$c],$nonce[$c],$adder[$c],$difficulty[$c],$shift[$c],$Merit6[$c],$Gap[$c],$gapstart[$c])|Add-Content "$($Path)$($DumpCustom)"}
-    Write-Warning "Final Clean Output path is '$($Path)$($DumpCustom)'"
+    Write-Warning "Final Clean Output path is $DumpCustom"
 
 
     #3/3 CONVERT CLEAN VARIABLES DATA INTO MERSENNE FORUM SUBMISSON FORMAT
@@ -77,12 +78,12 @@
     ###### Custom Format for Mersenne Forum  ######
     ###############################################
     #If MersenneForum DumpBlocks file exist, rename with formatted date
-    If ((Test-Path -Path "$($Path)$($DumpMersenne)" -PathType Leaf) -eq $True){
-    $Null=Rename-Item -Path "$($Path)$($DumpMersenne)" -NewName "$($Path)$($DumpMersenne)_$($FDate).csv" -Force -ErrorAction Ignore}
+    If ((Test-Path -Path $DumpMersenne -PathType Leaf) -eq $True){
+    $Null=Rename-Item -Path $DumpMersenne -NewName "$($DumpMersenne)_$($FDate).csv" -Force -ErrorAction Ignore}
     for($c = 0; $c -lt $height.Count; $c++){
     #If next is edited, no more for submission
-    ('{0}C??,{2},Gapcoin,{4}{5},{6}' -f $Gap[$c],'C??,',$Merit6[$c],'Gapcoin,',$Date[$c],$Digits[$c],$gapstart[$c])|Add-Content "$($Path)$($DumpMersenne)"}
-    Write-Warning "Final MersenneForum Output path is '$($Path)$($DumpMersenne)'"
+    ('{0}C??,{2},Gapcoin,{4}{5},{6}' -f $Gap[$c],'C??,',$Merit6[$c],'Gapcoin,',$Date[$c],$Digits[$c],$gapstart[$c])|Add-Content $DumpMersenne }
+    Write-Warning "Final MersenneForum Output path is $DumpMersenne"
 
 
     #5/5 CONVERT CLEAN VARIABLES DATA INTO S.Troisi SUBMISSION FORMAT
@@ -90,8 +91,8 @@
     ##### Custom Format for S.Troisi AutoSub  #####
     ###############################################
     #If Clean DumpBlocks file doesn't exist, create with headers
-    If ((Test-Path -Path "$($Path)$($DumpTroisi).csv" -PathType Leaf) -eq $False){
-    $Null=Add-Content -Path "$($Path)$($DumpTroisi).csv" -Value "Gap Date Discoverer Merit6 Gapstart"}
+    If ((Test-Path -Path $DumpTroisi -PathType Leaf) -eq $False){
+    $Null=Add-Content -Path $DumpTroisi -Value "Gap Date Discoverer Merit6 Gapstart"}
     #If next one is edited, no more for submission
-    "$gaplen $blockdates Gapcoin $Merit6 $gapstart"|Add-Content "$($Path)$($DumpTroisi).csv"
-    #Write-Warning "S.Troisi Format Output added to $DumpTroisi.csv"
+    "$gaplen $blockdates Gapcoin $Merit6 $gapstart"|Add-Content $DumpTroisi
+    Write-Warning "S.Troisi Format Output path is $DumpTroisi"
