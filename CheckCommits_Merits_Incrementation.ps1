9abd91fa6b782ccfb5deac9070c80b6f75a17809
Wizz_^ <lewizard@hotmail.fr>
2021-03-07 14:48:51 +0100

    Update CheckCommits_Merits_Incrementation.ps1

--- CheckCommits_Merits_Incrementation.ps1
+++ CheckCommits_Merits_Incrementation.ps1
@@ -1,5 +1,5 @@
 #Script to check Commits incrementation.
-#Git is required. Dump all commits (matching 'merit') content and Potentials errors.
+#Git is required. Dump all commits (matching 'merit') content and Potentials errors with infos.
 #Output files
 $file="C:\Temp\Test\TestCheckCommits\Potentials.txt"
 $all="C:\Temp\Test\TestCheckCommits\Commits.txt"
@@ -21,7 +21,7 @@ $com|Add-Content $all
 $modcom=$com|Select-String  \+INSERT,\-INSERT
 
 #Looking for each gap and compare to previous, if it's the same gap, else don't compare and next
-$previousgap=0;$previousmerit=0
+$previousgap=0;$previousmerit=0;$previousline=0
 Foreach($entry in $modcom){
 #Gap
 $gap=[decimal](($entry -split ' '|Select-String VALUES).ToString().Split(',')[0]).Split('(')[1]
@@ -31,11 +31,13 @@ Write-Host "Gap: $gap ; Merit: $merit"
 If ($Gap -eq $previousgap){
 #Check if current merit is smaller than previous, if yes dump infos
 If(($merit -lt $previousmerit)-eq $True){
-Write-Host "Potential error Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"
-"Potential error Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"|Add-Content $file }
+Write-Host "Potential error at Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"
+"Potential error at Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"|Add-Content $file
+"Introducted line: $entry"|Add-Content $file
+"Previous line: $previousline"|Add-Content $file }
 Else{
 #Saul Goodman
 Write-Host "Gap $Gap correctly incremented from $previousmerit to $merit"}}
 #Current to previous for next one
-$previousgap=$gap;$previousmerit=$merit}
+$previousgap=$gap;$previousmerit=$merit;$previousline=$entry}
 Write-Warning "Commit $line END, NEXT"}
