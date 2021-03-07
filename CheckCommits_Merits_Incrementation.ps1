de5886c728140c13bf0bfbb9a1cfbbe2a9416636
Wizz_^ <lewizard@hotmail.fr>
2021-03-07 14:27:26 +0100

    Update CheckCommits_Merits_Incrementation.ps1

--- CheckCommits_Merits_Incrementation.ps1
+++ CheckCommits_Merits_Incrementation.ps1
@@ -1,22 +1,24 @@
-﻿#Script to check Commits validity
+#Script to check Commits validity
 #Git is required. If all commits content isn't wanted, comment line 5
 #Output files
 $file="C:\Temp\Test\TestCheckCommits\Potentials.txt"
 $all="C:\Temp\Test\TestCheckCommits\Commits.txt"
-#Open Repo
+#Clone or Open Repo
+#git clone "https://github.com/primegap-list-project/prime-gap-list"
 cd C:\Users\Wizz\Documents\GitHub\prime-gap-list
 
-#Write all commits in a txt file with git
+#Get all commits matching merit in it
 $Commits=git log --oneline |Select-String merit
+#Start Loop
 $Changes=Foreach($line in $Commits){
 $line=$line -split ' ' #|Where{$_ -match "shift"}
 $Tag=$line[0]
-#Contient all the commit content
-$com=(git show $Tag --pretty=format:%b $COMMIT) |Add-Content $all
-#On récupère que les lignes avec '+' ou '-'
+#Commit content
+$com=(git show $Tag --pretty=format:%b $COMMIT)
+#Write all commit content into file
+$com|Add-Content $all
+#All changes in commit. On récupère que les lignes avec '+' ou '-'
 $modcom=$com|Select-String  \+INSERT,\-INSERT
-#Contient les toutes modifications du commit
-#$modcom
 
 #Looking for each gap and compare to previous, if it's the same gap, else don't compare and next
 $previousgap=0;$previousmerit=0
@@ -29,12 +31,12 @@ Write-Host "Gap: $gap ; Merit: $merit"
 If ($Gap -eq $previousgap){
 #Check if current merit is smaller than previous, if yes dump infos
 If(($merit -lt $previousmerit)-eq $True){
-Write-Host "Potential error Gap $Gap in commit '$Tag', le merite introduit: $merit < $previousmerit (Existing) '(git show $Tag --pretty=format:%b $Tag)'"
-"Potential error Gap $Gap in commit '$Tag', le merite introduit: $merit < $previousmerit (Existing) '(git show $Tag --pretty=format:%b $Tag)'"|Add-Content $file
+Write-Host "Potential error Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"
+"Potential error Gap $Gap in commit '$Tag', introducted merit: $merit < $previousmerit (Existing) Check:'(git show $Tag --pretty=format:%b $Tag)'"|Add-Content $file
 pause}
 Else{
 #Saul Goodman
-Write-Host "Le Gap $Gap est correctement incremente de $previousmerit a $merit"}}
+Write-Host "Gap $Gap correctly incremented from $previousmerit to $merit"}}
 #Current to previous for next one
 $previousgap=$gap;$previousmerit=$merit}
-Write-Warning "Commit $line End, NEXT"}
+Write-Warning "Commit $line END, NEXT"}
