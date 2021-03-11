47c2553850c5d4654fa8bbbb195b96c85a020b6a
Wizz_^ <lewizard@hotmail.fr>
2021-03-11 23:49:54 +0100

    Update [gapminer-cpu]RunCRTMultipleShiftsSuccessively.ps1

--- [gapminer-cpu]RunCRTMultipleShiftsSuccessively.ps1
+++ [gapminer-cpu]RunCRTMultipleShiftsSuccessively.ps1
@@ -1,4 +1,4 @@
-﻿#Variables: Starting shift ; Last shift +1 (excluded); Execution time for each shift, in sec
+﻿#Variables: Starting shift; Last shift +1 (excluded); Execution time for each shift, in sec
 [int]$i=1;$Last=31;$Timeout=$Seconds=42
 
 #Gapminer start-batch name (no extension) & dir. $Path is final output dir
