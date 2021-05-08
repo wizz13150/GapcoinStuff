68cc0423b9b5e1e8ec4aabb596b349dd68bd8e13
Wizz_^ <lewizard@hotmail.fr>
2021-05-08 13:00:17 +0200

    Update Dump_CompareToList.ps1

--- Dump_CompareToList.ps1
+++ Dump_CompareToList.ps1
@@ -1,4 +1,4 @@
-﻿#Script to compare gaps to the up to date PrimeGapList
+#Script to compare gaps to the up to date PrimeGapList
 #Comparison from a Dump formatted for Mersenne
 #ex: 4494,C??,22.551703,Gapcoin,01-03-2021,87,350115607822499236188157303964567234646664361596077138996270421912199500949152537724663
 
@@ -8,7 +8,7 @@ $Path="Z:\Gapcoin\Live Dump"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/primegap-list-project/prime-gap-list/master/allgaps.sql" -OutFile "$Path\LastPrimeGapList.txt"
 $PrimeGapList=Get-Content "$Path\LastPrimeGapList.txt"
 #Get Dump to Compare
-$ToCompare=Get-Content "$Path\Dump_Mersenne.csv"|Select-Object -Last 1000    #End of the file only
+$ToCompare=Get-Content "$Path\Dump_Mersenne.csv"|Select-Object -skip 1    #End of the file only. "-skip 1" for all except first line.
 #Create the PtentialRecords.txt, if doesn't exist
 If ((Test-Path -Path "$Path\PotentialRecords.txt" -PathType Leaf) -eq $False){
 $Null=Set-Content -Path "$Path\PotentialRecords.txt" -Value "Potential Gapcoin Records"}
@@ -30,9 +30,9 @@ $Merit=$Wanted[7]
 If($MerittoCompare -gt $Merit){
 $Diffe=$MerittoCompare - $Merit
 Write-Warning "Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"
-"Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"|Add-Content "$Path\PotentialRecords.txt"
+"Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"|Add-Content "C:\Temp\Test\ComparetoList\PotentialRecords.txt"
 $c++}Else{
-$Diff=$Merit - $MerittoCompare
-Write-Host "Fail. Gap $Gapsize avec un mérite de $Merit. Trouvé:$MerittoCompare Manque:$Diff ($s /"$ToCompare.count")"
+#$Diff=$Merit - $MerittoCompare
+#Write-Host "Fail. Gap $Gapsize avec un mérite de $Merit. Trouvé:$MerittoCompare Manque:$Diff ($s /"$ToCompare.count")"  #Comment to see only Potential Records
 }$s++
 }#End ForEach
