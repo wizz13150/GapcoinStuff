ca678096e2d4146be5126f9b103bf3c05b0f121d
Wizz_^ <lewizard@hotmail.fr>
2021-05-08 15:14:25 +0200

    Update Dump_CompareToList.ps1

--- Dump_CompareToList.ps1
+++ Dump_CompareToList.ps1
@@ -5,13 +5,15 @@
 #PrimeGapList and Files Path
 $Path="Z:\Gapcoin\Live Dump"
 #Download and Get the Prime Gap List
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/primegap-list-project/prime-gap-list/master/allgaps.sql" -OutFile "$Path\LastPrimeGapList.txt"
+#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/primegap-list-project/prime-gap-list/master/allgaps.sql" -OutFile "$Path\LastPrimeGapList.txt"
 $PrimeGapList=Get-Content "$Path\LastPrimeGapList.txt"
 #Get Dump to Compare
-$ToCompare=Get-Content "$Path\Dump_Mersenne.csv"|Select-Object -skip 1    #End of the file only. "-skip 1" for all except first line. "-last 100" for the last 100 lines.
+$ToCompare=Get-Content "$Path\Dump_Mersenne.csv"|Select-Object -Last 15       #End of the file only. "-skip 1" for all except first line. "-last 100" for the last 100 lines.
 #Create the PtentialRecords.txt, if doesn't exist
-If ((Test-Path -Path "$Path\PotentialRecords.txt" -PathType Leaf) -eq $False){
-$Null=Set-Content -Path "$Path\PotentialRecords.txt" -Value "Potential Gapcoin Records"}
+#If ((Test-Path -Path "$Path\PotentialRecords.txt" -PathType Leaf) -eq $False){
+#$Null=
+Set-Content -Path "$Path\PotentialRecords.txt" -Value "Potential Gapcoin Records"
+#}
 $c=1;$s=1;$TotalLines=$ToCompare.count
 
 #Look for each line to compare
@@ -30,9 +32,13 @@ $Merit=$Wanted[7]
 If($MerittoCompare -gt $Merit){
 $Diffe=$MerittoCompare - $Merit
 Write-Warning "Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"
-"Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"|Add-Content "C:\Temp\Test\ComparetoList\PotentialRecords.txt"
+Write-Warning "$line"
+"Le Gap $Gapsize est un Record potentiel avec un Mérite de $MerittoCompare. Existant=$Merit Difference:$Diffe ($c Record(s))"|Add-Content "$Path\PotentialRecords.txt"
+$line|Add-Content "$Path\PotentialRecords.txt"
+" "|Add-Content "$Path\PotentialRecords.txt"
 $c++}Else{
-#$Diff=$Merit - $MerittoCompare                                                                                          #Comment to see only Potential Records
-#Write-Host "Fail. Gap $Gapsize avec un mérite de $Merit. Trouvé:$MerittoCompare Manque:$Diff ($s /"$ToCompare.count")"  #Comment to see only Potential Records
+$Diff=$Merit - $MerittoCompare                                                                                          #Comment to see only Potential Records
+Write-Host "Fail. Gap $Gapsize avec un mérite de $Merit. Trouvé:$MerittoCompare Manque:$Diff ($s /"$ToCompare.count")"  #Comment to see only Potential Records
 }$s++
 }#End ForEach
+#pause
